79df7267e2d8fe4e384146d79f29edd25fc2152c
reditech5 <140551987+reditech5@users.noreply.github.com>
2023-11-09 22:26:08 +0500

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,70 +1,20 @@
 def filter_even_numbers(numbers):
-    """
-    Returns a list containing only the even numbers from the given list.
-
-    Input: 
-    numbers: a list of integers.
-
-    Output: 
-    A list containing only the even integers from the input list.
-
-    Example:
-    >>> filter_even_numbers([1, 2, 3, 4, 5, 6])
-    [2, 4, 6]
-    """
-    pass
+    return [n for n in numbers if n % 2 == 0]
 
 def sum_positive_numbers(numbers):
-    """
-    Returns the sum of all positive numbers in the list.
-
-    Input: 
-    numbers: a list of integers.
-
-    Output: 
-    An integer representing the sum of all positive integers in the input list.
-
-    Example:
-    >>> sum_positive_numbers([-1, 1, -2, 2, -3, 3])
-    6
-    """
-    pass
+    return sum([n for n in numbers if n > 0])
 
 def find_largest_string(strings):
-    """
-    Returns the largest string (by character count) from the given list.
-    If two strings have the same length, return the first one.
-    If the list is empty, return an empty string.
-
-    Input: 
-    strings: a list of strings.
-
-    Output: 
-    A string representing the longest string in the list.
-
-    Example:
-    >>> find_largest_string(["apple", "banana", "cherry"])
-    "banana"
-    """
-    pass
+    return max(strings, key=len, default='')
 
 def count_occurrences(numbers):
-    """
-    Returns a dictionary where the keys are numbers and the values are the number of times
-    each number appears in the given list.
-
-    Input: 
-    numbers: a list of integers.
-
-    Output: 
-    A dictionary where each key is an integer from the input list and its corresponding value is its count.
-
-    Example:
-    >>> count_occurrences([1, 2, 2, 3, 3, 3])
-    {1: 1, 2: 2, 3: 3}
-    """
-    pass
-
+    occurrences = {}
+    for n in numbers:
+        if n in occurrences:
+            occurrences[n] += 1
+        else:
+            occurrences[n] = 1
+    return occurrences
 
 def driver():
     while True:
@@ -79,8 +29,8 @@ def driver():
 
         if choice == "1":
             numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
-            print(filter_even_numbers(numbers))
-
+            filered_number = (filter_even_numbers(numbers))
+            print(filered_number)
         elif choice == "2":
             numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
             print(sum_positive_numbers(numbers))
